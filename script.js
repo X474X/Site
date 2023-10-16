56d70df4a610294144088eb2a07157b2bebbd2ed
X474X <133019572+X474X@users.noreply.github.com>
2023-10-17 00:25:02 +0300

    Added descriptions

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const projects = document.querySelectorAll(".link");
 for (let i = 0; i < projects.length; i++) {
   projects[i].addEventListener("click", () => {
     let current = document.querySelector(projects[i].getAttribute("goTo"));
+    current.scrollIntoView();
+    current.scrollIntoView({
+      behavior: "smooth",
+    });
     console.log(current);
   });
 }
