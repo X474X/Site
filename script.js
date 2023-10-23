c20d5fd001347762f47f1dda008f61d03a499e80
X474X <133019572+X474X@users.noreply.github.com>
2023-10-23 18:17:01 +0300

    UI testing for mobile responsivness

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 const projects = document.querySelectorAll(".link");
+const bars = document.querySelectorAll(".bar");
+const targetElement = document.querySelector(".tehnologies");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      barAnimation();
+    }
+  });
+});
 
 for (let i = 0; i < projects.length; i++) {
   projects[i].addEventListener("click", () => {
     let current = document.querySelector(projects[i].getAttribute("goTo"));
-    current.scrollIntoView();
     current.scrollIntoView({
       behavior: "smooth",
+      block: "start",
     });
-    console.log(current);
+    if (projects[i].getAttribute("goTo") === ".tehnologies") {
+      barAnimation();
+    }
   });
 }
 
-//https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+const barAnimation = () => {
+  for (let i = 0; i < bars.length; i++) {
+    setTimeout(() => {
+      bars[i].style.width = bars[i].getAttribute("percent");
+      setTimeout(() => {
+        bars[i].innerHTML = bars[i].getAttribute("percent");
+      }, 500);
+    }, i * 800);
+  }
+};
+
+observer.observe(targetElement);
